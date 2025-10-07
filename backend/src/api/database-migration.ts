2edcc00e6b42738ab8e5d90ed3b9882604914e08
Zach Price <37559274+zachchan105@users.noreply.github.com>
2025-10-07 01:04:26 -0700

    increase header size to 4000

--- backend/src/api/database-migration.ts
+++ backend/src/api/database-migration.ts
@@ -812,7 +812,7 @@ class DatabaseMigration {
       ADD segwit_total_txs int unsigned NOT NULL,
       ADD segwit_total_size int unsigned NOT NULL,
       ADD segwit_total_weight int unsigned NOT NULL,
-      ADD header varchar(2000) NOT NULL,
+      ADD header varchar(4000) NOT NULL,
       ADD utxoset_change int NOT NULL,
       ADD utxoset_size int unsigned NULL,
       ADD total_input_amt bigint unsigned NULL
